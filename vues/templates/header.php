694b2a4e089791af36b9bb2be6ef30ab7567446a
JulianRendon <julian_javier@hotmail.com>
2014-10-23 10:13:33 -0400

    Ajout titre dans l'entete

--- vues/templates/header.php
+++ vues/templates/header.php
@@ -14,7 +14,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Redwood | Julian Rendon</title>
+		<title>nom du projet | Julian Rendon</title>
 		<meta name="author" content="Julian Rendon">
 
 		<!-- Bootstrap CSS -->
