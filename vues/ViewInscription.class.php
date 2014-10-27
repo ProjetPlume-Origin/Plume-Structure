96f3ffa5746b4ff34c34fcc0d1b8a4941366714d
JulianRendon <julian_javier@hotmail.com>
2014-10-26 20:16:23 -0400

    ajout de tableau dans la vue Inscription cotê Admin

--- vues/ViewInscription.class.php
+++ vues/ViewInscription.class.php
@@ -77,56 +77,63 @@
         
   /*--------------------------------------------------------------------------------------------------------------------------*/      
         
-   public static function afficherListeUtilisateurs(array $aUtilisateurs , $sMsg="&nbsp;"){
+   		public static function afficherListeUtilisateurs(array $aUtilisateurs , $sMsg="&nbsp;"){
 			//var_dump($aUtilisateurs);
 			
 			echo "
-				<h1>Liste des Utilisateurs  <a href=\"index.php?s=".$_GET['s']."&action=add\">Ajouter <img src=\"../core/img/ajouter.png\" width=\"25\" height=\"25\"></a> </h1>
-				<p>".$sMsg."</p>";
+				<h1>Liste des Utilisateurs  <a href=\"index.php?s=".$_GET['s']."&action=add\">Ajouter <img src=\"../core/img/ajouter.png\" width=\"15\" height=\"15\"></a> </h1>
+				<p class=\"alert alert-success\">".$sMsg."</p>
+				";
 				if(empty($aUtilisateurs)!=true){
-					echo 
-						"
-						<table >
-							<tr>
-                                <th>Nom</th>
-                                <th>Courriel</th>
-                                <th>mot de passe </th>
-                                <th>Status</th>
-                                <th>Type Utilisateur</th>
-                                <th>Avatar</th>
-                            </tr>
+					echo "
+						<div class=\"table-responsive\">
+							<table class=\"table\">
+								<thead>
+						        	<tr>
+		                                <th>Nom</th>
+		                                <th>Courriel</th>
+		                                <th>mot de passe </th>
+		                                <th>Status</th>
+		                                <th>Type Utilisateur</th>
+		                                <th>Avatar</th>
+		                                <th>Action</th>
+		                            </tr>
+	                            </thead>
+				      			<tbody>
 						";
                    for($iEnrg = 0; $iEnrg<count($aUtilisateurs); $iEnrg++){
 						echo "
-					   <tr>
-                            <th> ".$aUtilisateurs[$iEnrg]->getNom()." </th>
-                            <th>".$aUtilisateurs[$iEnrg]->getCourriel()." </th>
-                            <th>".$aUtilisateurs[$iEnrg]->getMotDePasse()." </th>
-                            <th>".$aUtilisateurs[$iEnrg]->getStatus()." </th>
-                            <th>".$aUtilisateurs[$iEnrg]->getTypeUtilisateur()." </th>
-                            <th>".$aUtilisateurs[$iEnrg]->getAvatar()." </th>
-                           <th><a href=\"index.php?s=".$_GET['s']."&action=mod&idUtilisateur=".$aUtilisateurs[$iEnrg]->getIdUtilisateur()."\"><img src=\"../core/img/modifier.png\" width=\"25\" height=\"25\"></a></th>
-						<th><a href=\"#\" onclick=\"supprimerUnUtilisateur('Voulez-vous supprimer ce Utilisateurt', ".$_GET['s'].", 'sup',".$aUtilisateurs[$iEnrg]->getIdUtilisateur().")\"><img src=\"../core/img/supprimer.png\" width=\"25\" height=\"25\"></a> </th></tr>
-					";
+					   		<tr>
+	                            <td>".$aUtilisateurs[$iEnrg]->getNom()." </td>
+	                            <td>".$aUtilisateurs[$iEnrg]->getCourriel()." </td>
+	                            <td>".$aUtilisateurs[$iEnrg]->getMotDePasse()." </td>
+	                            <td>".$aUtilisateurs[$iEnrg]->getStatus()." </td>
+	                            <td>".$aUtilisateurs[$iEnrg]->getTypeUtilisateur()." </td>
+	                            <td>".$aUtilisateurs[$iEnrg]->getAvatar()." </td>
+                           		<td><a href=\"index.php?s=".$_GET['s']
+                           		."&action=mod&idUtilisateur=".$aUtilisateurs[$iEnrg]->getIdUtilisateur()."\"><img src=\"../core/img/modifier.png\" width=\"15\" height=\"15\"></a> |
+
+                           		<a href=\"#\" onclick=\"supprimerUnUtilisateur('Voulez-vous supprimer ce Utilisateurt', ".$_GET['s'].", 'sup',".$aUtilisateurs[$iEnrg]->getIdUtilisateur().")\">
+                           		<img src=\"../core/img/supprimer.png\" width=\"15\" height=\"15\"></a> </td>
+                       		</tr>
+							";
                     }
-					echo '</table>';
+					echo "								
+			         			</tbody>
+	   						</table>
+						</div>				
+						";
 					}else{
 						echo '<p>Aucun utilisateur n’est disponible à l’heure actuelle. Vous pouvez en ajouter un.</p>';
 				    }
 					
-		}
-        
-        
-        
-        
-        
+		} //fin de la fonction afficherListeUtilisateurs()
+
         
         /*<th>".$aUtilisateurs[$iEnrg]->getTypeUtilisateur()." </th>
                                    <th>".$aUtilisateurs[$iEnrg]->getStatut()." </th>*/
         
         
-        
-        
-    }
+    } // Fin de la classe ViewInscription()
 
 ?>
