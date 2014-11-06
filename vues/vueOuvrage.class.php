fd04b7b8956880b4a37cf1663f89cb48b8862516
jllkhair <khairjll@hotmail.com>
2014-11-05 19:29:13 -0500

    effacer BR

--- vues/vueOuvrage.class.php
+++ vues/vueOuvrage.class.php
@@ -301,7 +301,7 @@ class VueOuvrage{
     echo "Genre : ".$oOuvrage->getOuvrageGenre()."<br><br><br>";
     if (isset($_SESSION['tContenu'])){
       foreach ($_SESSION['tContenu'] as $valeur ) {
-        echo "<p>".$valeur."</p><br>";
+        echo "<p>".$valeur."</p>";
       }
     }
     echo "</article>";
