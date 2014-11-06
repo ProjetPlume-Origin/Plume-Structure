5cdde33081d4da063b25caac7d7726d6fa6b0ebe
hanaa121 <elhamoumi_hanaa@hotmail.fr>
2014-11-06 16:27:59 -0500

    ajout gererUtilisateurSite

---
+++ ajax/gererUtilisateurSite.php
@@ -0,0 +1,31 @@
+<?php
+    /**
+	 * Inclure les librairies
+	 */	
+	 require_once("../lib/TypeException.class.php");
+	 require_once("../lib/MySqliException.class.php");
+	 require_once("../lib/MySqliLib.class.php");
+	 /**
+	 * Inclure les modèles
+	 */
+	 require_once("../modeles/Utilisateur.class.php");
+     
+    require_once("../vues/ViewInscription.class.php");
+	 /**
+	 * Inclure le contrôleur
+	 */
+	 require_once("../site/Controleur.class.php");
+    
+   // $bDelete = Controleur::gererSupprimerUtilisateurAdmin();
+	//header("Location:../core/index.php?s=".$_GET['s']."&bSup=".$bDelete);
+
+
+    try{		
+		Controleur::ajax_gererUtilisateur();
+	}catch(Exception $e){
+		echo $e->getMessage();
+	}
+
+
+
+?>
